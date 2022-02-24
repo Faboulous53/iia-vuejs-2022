47eed603fdc1f940a003150f9b2c06bdeeb68f6e
Jeremy <jeremy.perrouault@ascadis.fr>
2022-02-24 12:31:46 +0100

    JOUR1 - Binding & Events & If

--- formation-vuejs/src/App.vue
+++ formation-vuejs/src/App.vue
@@ -1,15 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <div>
+      Hello {{ utilisateur.username }}, vous avez {{ utilisateur.age }} ans !
+      <div>
+        <input type="text" v-model="utilisateur.username" />
+        <input type="number" v-model="utilisateur.age" />
+      </div>
+
+      <!-- v-on:eventName -->
+      <!-- @eventName -->
+      <button @click="demoClick()">Reset Username</button>
+      <button @click="resetAge(10)">Reset Age</button>
+
+      <!-- IF : Afficher "Majeur" SI age >= 18, SINON afficher "Mineur" -->
+      <!-- Deux paragraphes, qui s'afficheront ou pas en fonction de l'age -->
+      <!-- <p v-if="utilisateur.age >= 18">Majeur</p> -->
+      <p v-if="isMajeur()">Majeur</p>
+      <p v-else>Mineur</p>
+
+      <p>{{ strMajeur() }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  
+  // data => Données
+  data() {
+    // Nos données, utilisables dans notre template
+    return {
+      // utilisateur: "Jérémy"
+      utilisateur: {
+        username: "Jérémy",
+        age: 28
+      }
+    }
+  },
+
+  // methods => les fonctionnalités
+  methods: {
+    demoClick() {
+      // alert("On a cliqué ici ...");
+      // Accès au contexte du fichier Vue avec le mot-clé 'this'
+      this.utilisateur.username = "Défaut";
+    },
+
+    resetAge(age) {
+      this.utilisateur.age = age;
+    },
+
+    isMajeur() {
+      return (this.utilisateur.age >= 18);
+    },
+
+    strMajeur() {
+      // if (this.utilisateur.age >= 18) {
+      //   return "MAJEUR";
+      // }
+
+      // return "MINEUR";
+
+      return (this.utilisateur.age >= 18) ? "MAJEUR" : "MINEUR";
+    }
   }
 }
 </script>
