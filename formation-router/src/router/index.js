b70fcf7ad6fdf1255f76a3047006759863962273
Jeremy <jeremy.perrouault@ascadis.fr>
2022-02-25 15:00:38 +0100

    JOUR 2 - TODO LIST

--- formation-router/src/router/index.js
+++ formation-router/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Demo from '../views/Demo.vue'
+import TodoView from '../views/TodoView.vue'
 
 const routes = [
   {
@@ -13,6 +14,12 @@ const routes = [
     path: '/demo',
     name: 'Demo',
     component: Demo
+  },
+
+  {
+    path: '/todo',
+    name: 'todo',
+    component: TodoView
   },
 
   {
---
+++ formation-router/src/views/TodoView.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<h1>TODO LIST</h1>
+
+		<todo	v-for="todo of todos"
+				:key="todo.id"
+				:todo="todo" />
+	</div>
+</template>
+
+<script>
+// @ fait revenir au répertoire "src"
+// import Todo from '../components/TodoComponent.vue';
+import Todo from '@/components/TodoComponent.vue';
+
+export default {
+	components: {
+		Todo
+	},
+
+	data() {
+		return {
+			todos: [
+				{ id: 1, title: "OK", completed: false },
+				{ id: 2, title: "OK 2", completed: true },
+				{ id: 3, title: "OK 3", completed: false },
+			]
+		}
+	}
+}
+</script>
