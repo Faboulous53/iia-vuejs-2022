653d760349a9e5371660098bfcdfeb8257f718a5
Jeremy <jeremy.perrouault@ascadis.fr>
2022-02-25 13:49:22 +0100

    JOUR 2 - Page Demo

--- formation-router/src/router/index.js
+++ formation-router/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Demo from '../views/Demo.vue'
 
 const routes = [
   {
@@ -7,6 +8,13 @@ const routes = [
     name: 'Home',
     component: Home
   },
+
+  {
+    path: '/demo',
+    name: 'Demo',
+    component: Demo
+  },
+
   {
     path: '/about',
     name: 'About',
